54767665853834d7b25bfa7683720c0b98fe5645
Eugene Domanovich <dompro1994.ed@gmail.com>
2023-12-01 17:15:38 +0300

    ad

---
+++ src/App.vue
@@ -0,0 +1,163 @@
+<script setup lang="ts">
+import { ref, onMounted, computed } from "vue";
+import { API_KEY, BASE_URL } from "./constants/api.ts";
+import { capitalizeFirstLetter } from "./utils";
+import WeatherSummary from "./components/WeatherSummary.vue";
+import Highlights from "./components/Highlights.vue";
+import Coords from "./components/Coords.vue";
+import Humidity from "./components/Humidity.vue";
+
+const city = ref("Minsk");
+const weatherInfo = ref(null);
+
+const isError = computed(() => {
+  return weatherInfo.value?.cod !== 200;
+});
+
+const getWeather = () => {
+  fetch(`${BASE_URL}?q=${city.value}&units=metric&appid=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => (weatherInfo.value = data));
+};
+
+onMounted(getWeather);
+</script>
+
+<template>
+  <div class="page">
+    <main class="main">
+      <div class="container">
+        <div class="laptop">
+          <div class="sections">
+            <section class="section section-left">
+              <div class="info">
+                <div class="city-inner">
+                  <input
+                    v-model="city"
+                    @keyup.enter="errorMessagegetWeather"
+                    type="text"
+                    class="search"
+                  />
+                </div>
+                <WeatherSummary v-if="!isError" :weatherInfo="weatherInfo" />
+                <div v-else class="error">
+                  <div class="isError">Oooops! Where your wrong</div>
+                  <div v-if="weatherInfo?.message" class="errorMessage">
+                    {{ capitalizeFirstLetter(weatherInfo?.message) }}
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section v-if="weatherInfo" class="section section-right">
+              <Highlights :weatherInfo="weatherInfo" />
+            </section>
+          </div>
+          <div v-if="weatherInfo?.weather" class="sections">
+            <Coords :coords="weatherInfo.coord" />
+            <Humidity :humidity="weatherInfo.main" />
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "./assets/styles/main.scss";
+.page {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding: 20px 0;
+  background-color: #59585d;
+}
+.laptop {
+  width: 100%;
+  padding: 20px;
+  background-color: #0e100f;
+  border-radius: 25px;
+}
+.sections {
+  display: flex;
+  width: 100%;
+
+  @media (max-width: 767px) {
+    flex-direction: column;
+  }
+}
+.section-left {
+  width: 30%;
+  padding-right: 10px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    padding-right: 0;
+  }
+}
+.section-right {
+  width: 70%;
+  padding-left: 10px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    margin-top: 16px;
+    padding-left: 0;
+  }
+}
+.city-inner {
+  position: relative;
+  display: inline-block;
+  width: 100%;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 10px;
+    width: 25px;
+    height: 25px;
+    background: url("./assets/img/search.svg") no-repeat 50% 50%;
+    background-size: contain;
+    transform: translateY(50%);
+    cursor: pointer;
+  }
+}
+
+.error {
+  .isError {
+    font-size: 20px;
+    font-weight: 800;
+  }
+  .errorMessage {
+  }
+}
+.info {
+  height: 100%;
+  padding: 16px;
+  background: url("./assets/img/gradient-1.jpg") no-repeat 50% 50%;
+  background-size: cover;
+  border-radius: 25px;
+}
+.search {
+  width: 100%;
+  padding: 16px;
+  font-family: "Inter", Arial, sans-serif;
+  color: $white;
+  background-color: rgba(0, 0, 0, 0.75);
+  border-radius: 16px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+.section-bottom {
+  width: 50%;
+  margin-top: 16px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+  }
+}
+</style>
+./constants/api.js
---
+++ src/constants/api.ts
@@ -0,0 +1,2 @@
+export const API_KEY = "213a4637d3dd83cc96afa8d585a84c4d";
+export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
